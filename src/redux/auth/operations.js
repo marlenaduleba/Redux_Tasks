99f3aef67a4d9e49841548dafd38ef43622fe51e
Marlena Dulęba <marlena_duleba@o2.pl>
2023-05-10 18:07:11 +0200

    token header fixed

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,7 +4,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
 
 const setAuthHeader = token => {
-  axios.defaults.headers.common.Authorization = `Baerer ${token}`;
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearAuthHeader = () => {
